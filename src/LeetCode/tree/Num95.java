6fd124ca3e6038c275fbfebbeb7f4587641fe82e
tuzhijing <tucosmos@foxmail.com>
2021-02-22 13:33:33 +0800

    Num95

--- src/LeetCode/tree/Num95.java
+++ src/LeetCode/tree/Num95.java
@@ -1,16 +1,36 @@
 package LeetCode.tree;
 
+import javax.xml.soap.Node;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.ListIterator;
 
 public class Num95 {
     public List<TreeNode> generateTrees(int n) {
-        List<TreeNode> list=new ArrayList<>();
-        int a[]=new int[n];
-
-        return list;
+        return dp(1,n);
     }
 
+    //递归
+    public List<TreeNode> dp(int start,int end){
+        List<TreeNode> list=new LinkedList<>();
+        if(start>end){
+            list.add(null);
+            return list;
+        }
+        for(int i=start;i<=end;i++){
+            List<TreeNode> left=dp(start,i-1);
+            List<TreeNode> right=dp(i+1,end);
+            for(int j=0;j<left.size();j++){
+                for(int k=0;k<right.size();k++){
+                    TreeNode node=new TreeNode(i);
+                    node.left=left.get(j);
+                    node.right=right.get(k);
+                    list.add(node);
+                }
+            }
+        }
+        return list;
+    }
 
 }
