a081a9e4c1b1d73e1b879220811cfb47a5e6dbd0
cosmos <alberto.tu@oktech.sg>
2021-07-15 09:08:27 +0800

    Num169

---
+++ src/LeetCode/sequence/Num169.java
@@ -0,0 +1,29 @@
+package LeetCode.sequence;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Num169 {
+    public int majorityElement(int[] nums) {
+        int len=nums.length;
+        Map<Integer,Integer> map=new HashMap<>(len);
+        for(int i=0;i<len;i++){
+            if(map.get(nums[i])==null){
+                map.put(nums[i],1);
+            }else{
+                Integer val=map.get(nums[i]);
+                if(val+1>len/2){
+                    return nums[i];
+                }
+                map.put(nums[i],val+1);
+            }
+        }
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Num169 num169=new Num169();
+        int[] a={3,2,3};
+        System.out.println(num169.majorityElement(a));
+    }
+}
